8c89dee49da875a6eeffc5c025bed13410ccdb7d
briPod <33463220+br1Pod@users.noreply.github.com>
2021-09-27 15:51:48 +0100

    add, subtract, multiply and divide methods written... tests passing

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,11 +1,31 @@
 public class Calculator {
 
-    private double value1;
-    private double value2;
+    private int num1;
+    private int num2;
+    private double number1;
+    private double number2;
 
-    public Calculator(double value1, double value2) {
-        this.value1 = value1;
-        this.value2 = value2;
+    public Calculator() {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.number1 = number1;
+        this.number2 = number2;
+    }
+
+    public int add(int num1, int num2){
+        return num1 + num2;
+    }
+
+    public int subtract(int num1, int num2){
+        return num1 - num2;
+    }
+
+    public int multiply(int num1, int num2){
+        return num1 * num2;
+    }
+
+    public double divide(double number1, double number2){
+        return number1 / number2;
     }
 
 
--- src/test/java/TestCalculator.java
+++ src/test/java/TestCalculator.java
@@ -1,2 +1,34 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class TestCalculator {
+
+    Calculator calculator;
+
+    @Before
+    public void before(){
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void canAdd(){
+        assertEquals(5, calculator.add(2,3));
+    }
+
+    @Test
+    public void canSubtract(){
+        assertEquals(3, calculator.subtract(10, 7));
+    }
+
+    @Test
+    public void canMultiply(){
+        assertEquals(42, calculator.multiply(6, 7));
+    }
+
+    @Test
+    public void canDivide(){
+        assertEquals(3.5, calculator.divide(7.0, 2.0), 0.01);
+    }
+
 }
