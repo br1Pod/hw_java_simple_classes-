725d99c1d74eab9d31ef5e130cf7fd904d2852ff
briPod <33463220+br1Pod@users.noreply.github.com>
2021-09-27 13:50:22 +0100

    Calculator class created

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator {
+
+    private double value1;
+    private double value2;
+
+    public Calculator(double value1, double value2) {
+        this.value1 = value1;
+        this.value2 = value2;
+    }
+
+
+}
