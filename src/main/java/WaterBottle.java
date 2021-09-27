f515fccf424117dba0baaf8603a9e1f0754d46b4
briPod <33463220+br1Pod@users.noreply.github.com>
2021-09-27 16:39:41 +0100

    WaterBottle class created, checkVolume written and passing, drink written and passing

---
+++ src/main/java/WaterBottle.java
@@ -0,0 +1,16 @@
+public class WaterBottle {
+
+    private static int volume;
+
+    public WaterBottle(){
+        this.volume = volume = 100;
+    }
+
+    public int volume(){
+        return this.volume;
+    }
+
+    public int drink(){
+        return this.volume - 10;
+    }
+}
---
+++ src/test/java/TestWaterBottle.java
@@ -0,0 +1,23 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestWaterBottle {
+
+    WaterBottle waterBottle;
+
+    @Before
+    public void before(){
+        waterBottle = new WaterBottle();
+    }
+
+    @Test
+    public void checkVolume(){
+        assertEquals(100, waterBottle.volume());
+    }
+
+    @Test
+    public void checkDrinkReducesVolume(){
+        assertEquals(90, waterBottle.drink());
+    }
+}
